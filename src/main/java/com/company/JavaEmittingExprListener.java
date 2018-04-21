11bdfc03811fd0bba2bbf30d9e294547cbabe896
Samuel Moritz <samuel.moritz@gmail.com>
2018-04-21 21:02:31 +0200

    Separate generated code

--- src/main/java/com/company/JavaEmittingExprListener.java
+++ src/main/java/com/company/JavaEmittingExprListener.java
@@ -8,81 +8,80 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
+import com.generated.*;
 
 public class JavaEmittingExprListener extends ExprBaseListener {
-//
-//	String output = "";
-//
-//	@Override
-//	public void enterProg(ExprParser.ProgContext ctx) {
-//		super.enterProg(ctx);
-//		output += "public class Foo {\n";
-//		output += "public static void main(String[] args) {\n";
-//
-//	}
-//
-//	@Override
-//	public void exitProg(ExprParser.ProgContext ctx) {
-//		super.exitProg(ctx);
-//		output += "}\n";
-//		output += "}\n";
-//
-//		System.out.println(output);
-//		try {
-//			FileWriter fileWriter = new FileWriter("Foo.java");
-//			fileWriter.write(output);
-//			fileWriter.close();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//
-//		File[] files1 = {new File("Foo.java")} ; // input for first compilation task
-//		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-//		StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
-//		Iterable<? extends JavaFileObject> compilationUnits1 =            fileManager.getJavaFileObjectsFromFiles(Arrays.asList(files1));
-//		Boolean compilerResult = compiler.getTask(null, fileManager, null, null, null, compilationUnits1).call();
-//		System.out.println(compilerResult);
-//	}
-//
-//	@Override
-//	public void enterAssignment(ExprParser.AssignmentContext ctx) {
-//		super.enterAssignment(ctx);
-//		String variableName = ctx.getChild(1).getText();
-//		String value = ctx.getChild(5).getText();
-//		output += "String " + variableName + " = " + value + ";\n";
-//	}
-//
-//	@Override
-//	public void exitAssignment(ExprParser.AssignmentContext ctx) {
-//		super.exitAssignment(ctx);
-//	}
-//
-//	@Override
-//	public void enterPrintStatement(ExprParser.PrintStatementContext ctx) {
-//		super.enterPrintStatement(ctx);
-//		output += "System.out.println(" + ctx.getChild(1).getText() +");\n";
-//	}
-//
-//	@Override
-//	public void exitPrintStatement(ExprParser.PrintStatementContext ctx) {
-//		super.exitPrintStatement(ctx);
-//	}
-//
-//	@Override
-//	public void enterExternalFunction(ExprParser.ExternalFunctionContext ctx) {
-//		super.enterExternalFunction(ctx);
-//		output += ctx.IMPORTED_IDENTIFIER();
-//		output += "(";
-//		output += ctx.arg().stream().map(argContext -> argContext.getText()).collect(Collectors.joining(","));
-//		output += ");";
-//		output += "\n";
-//	}
-//
-//	@Override
-//	public void exitExternalFunction(ExprParser.ExternalFunctionContext ctx) {
-//		super.exitExternalFunction(ctx);
-//	}
+
+	String output = "";
+
+	@Override
+	public void enterProg(ExprParser.ProgContext ctx) {
+		super.enterProg(ctx);
+		output += "public class Foo {\n";
+		output += "public static void main(String[] args) {\n";
+
+	}
+
+	@Override
+	public void exitProg(ExprParser.ProgContext ctx) {
+		super.exitProg(ctx);
+		output += "}\n";
+		output += "}\n";
+
+		System.out.println(output);
+		try {
+			FileWriter fileWriter = new FileWriter("Foo.java");
+			fileWriter.write(output);
+			fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		File[] files1 = {new File("Foo.java")} ; // input for first compilation task
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+		Iterable<? extends JavaFileObject> compilationUnits1 =            fileManager.getJavaFileObjectsFromFiles(Arrays.asList(files1));
+		Boolean compilerResult = compiler.getTask(null, fileManager, null, null, null, compilationUnits1).call();
+		System.out.println(compilerResult);
+	}
+
+	@Override
+	public void enterAssignment(ExprParser.AssignmentContext ctx) {
+		super.enterAssignment(ctx);
+		String variableName = ctx.getChild(1).getText();
+		String value = ctx.getChild(5).getText();
+		output += "String " + variableName + " = " + value + ";\n";
+	}
+
+	@Override
+	public void exitAssignment(ExprParser.AssignmentContext ctx) {
+		super.exitAssignment(ctx);
+	}
+
+	@Override
+	public void enterPrintStatement(ExprParser.PrintStatementContext ctx) {
+		super.enterPrintStatement(ctx);
+		output += "System.out.println(" + ctx.getChild(1).getText() +");\n";
+	}
+
+	@Override
+	public void exitPrintStatement(ExprParser.PrintStatementContext ctx) {
+		super.exitPrintStatement(ctx);
+	}
+
+	@Override
+	public void enterExternalFunction(ExprParser.ExternalFunctionContext ctx) {
+		super.enterExternalFunction(ctx);
+		output += ctx.IMPORTED_IDENTIFIER();
+		output += "(";
+		output += ctx.arg().stream().map(argContext -> argContext.getText()).collect(Collectors.joining(","));
+		output += ");";
+		output += "\n";
+	}
+
+	@Override
+	public void exitExternalFunction(ExprParser.ExternalFunctionContext ctx) {
+		super.exitExternalFunction(ctx);
+	}
 }
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -7,14 +7,16 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import java.io.IOException;
 
+import com.generated.*;
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
-//		ExprLexer lexer = new ExprLexer(new ANTLRFileStream("example.foo"));
-//		CommonTokenStream tokens = new CommonTokenStream( lexer );
-//		ExprParser parser = new ExprParser( tokens );
-//		ParseTree tree = parser.prog();
-//		ParseTreeWalker walker = new ParseTreeWalker();
-//		walker.walk( new JavaEmittingExprListener(), tree );
+		ExprLexer lexer = new ExprLexer(new ANTLRFileStream("example.foo"));
+		CommonTokenStream tokens = new CommonTokenStream( lexer );
+		ExprParser parser = new ExprParser( tokens );
+		ParseTree tree = parser.prog();
+		ParseTreeWalker walker = new ParseTreeWalker();
+		walker.walk( new JavaEmittingExprListener(), tree );
     }
 }
