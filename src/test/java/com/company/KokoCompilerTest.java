72cf210e7a187c59a998f1027d09cf42a109f867
Samuel Moritz <samuel.moritz@gmail.com>
2018-04-22 21:00:08 +0200

    Start from scratch with empty grammar

--- src/test/java/com/company/KokoCompilerTest.java
+++ src/test/java/com/company/KokoCompilerTest.java
@@ -4,15 +4,26 @@ import org.junit.Test;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class KokoCompilerTest {
 
     @Test
-    public void shouldCompileEmptyString() throws Exception {
+    public void shouldGenerateClassfile() throws Exception {
         Class clazz = new KokoCompiler().compile("");
         assertNotNull(clazz);
+    }
+
+    @Test
+    public void shouldExportFunction() {
+        Class clazz = new KokoCompiler().compile("myfunc void");
+        List<Method> methods = Arrays.asList(clazz.getDeclaredMethods());
+        assertEquals(1,methods.size());
+        assertEquals("myfunc",methods.get(0).getName());
     }
 
     @Test
